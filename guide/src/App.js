47d33a0402e2d85776cb5e7695aa5bd7503594c1
kotov <kotov.nikita.sergeevich@gmail.com>
2020-01-04 22:16:10 +0300

    Using state and changin state in statefull component

--- guide/src/App.js
+++ guide/src/App.js
@@ -4,14 +4,45 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  // "state" is a special name in statefull component to keep current state
+  state = {
+    persons: [
+      {name: "Nikita", age: 25},
+      {name: "Boba", age: 23},
+      {name: "Banksy", age: 3}
+    ],
+    info: "Very important info",
+  }
+
+  // Arrow function should be used to execute method with correct context
+  changeNamesHandler = () => {
+    // 'setState' is a special function that must be used to change 'state'
+    // it merges new state with previous (info: "Very important info" won't be changed)
+    this.setState({
+      persons: [
+        {name: "NIKITA", age: 25},
+        {name: "BOBA", age: 23},
+        {name: "BANKSY", age: 3}
+      ],
+    })
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
+          <button onClick={this.changeNamesHandler}>Uppercase Names</button>
           {/* Adding component just as an HTML element */}
-          <Person name="Nikita" age="25"/>
-          <Person name="Boba" age="23"/>
-          <Person name="Banksy" age="4"/>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}/>
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}/>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
         </header>
       </div>
     );
