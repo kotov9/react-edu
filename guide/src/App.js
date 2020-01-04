c485c2e8b2aba4a6b0a7ad6b9a9741e6b3fedd5d
kotov <kotov.nikita.sergeevich@gmail.com>
2020-01-04 22:30:05 +0300

    Making stateless component statefull using 'useState'

--- guide/src/App.js
+++ guide/src/App.js
@@ -1,25 +1,26 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 // Import stateless component "person"
 import Person from './Person/Person';
 
-class App extends Component {
+// Stateless component can be turned into stateful using 'useState' function
+const App = () => {
 
-  // "state" is a special name in statefull component to keep current state
-  state = {
+  // useState returns 'personState' and function to set a new 'personState'
+  const [personState, personStateSetter] = useState({
     persons: [
       {name: "Nikita", age: 25},
       {name: "Boba", age: 23},
       {name: "Banksy", age: 3}
     ],
     info: "Very important info",
-  }
+  })
 
   // Arrow function should be used to execute method with correct context
-  changeNamesHandler = () => {
-    // 'setState' is a special function that must be used to change 'state'
-    // it merges new state with previous (info: "Very important info" won't be changed)
-    this.setState({
+  const changeNamesHandler = () => {
+    // 'personStateSetter' is a function that must be used to change 'personState'
+    // it create a new state (info: "Very important info" will be removed)
+    personStateSetter({
       persons: [
         {name: "NIKITA", age: 25},
         {name: "BOBA", age: 23},
@@ -28,25 +29,23 @@ class App extends Component {
     })
   }
 
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <button onClick={this.changeNamesHandler}>Uppercase Names</button>
-          {/* Adding component just as an HTML element */}
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age}/>
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}/>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age}/>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={changeNamesHandler}>Uppercase Names</button>
+        {/* Adding component just as an HTML element */}
+        <Person 
+          name={personState.persons[0].name} 
+          age={personState.persons[0].age}/>
+        <Person 
+          name={personState.persons[1].name} 
+          age={personState.persons[1].age}/>
+        <Person 
+          name={personState.persons[2].name} 
+          age={personState.persons[2].age}/>
+      </header>
+    </div>
+  );
 }
 
 export default App;
