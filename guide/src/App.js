602ac74ee6cde6f99842e1a4743d2cc1f3732a41
kotov <kotov.nikita.sergeevich@gmail.com>
2020-01-04 22:43:41 +0300

    Dinamically change output if changing input

--- guide/src/App.js
+++ guide/src/App.js
@@ -1,26 +1,25 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 // Import stateless component "person"
 import Person from './Person/Person';
 
-// Stateless component can be turned into stateful using 'useState' function
-const App = () => {
+class App extends Component {
 
-  // useState returns 'personState' and function to set a new 'personState'
-  const [personState, personStateSetter] = useState({
+  // "state" is a special name in statefull component to keep current state
+  state = {
     persons: [
       {name: "Nikita", age: 25},
       {name: "Boba", age: 23},
       {name: "Banksy", age: 3}
     ],
     info: "Very important info",
-  })
+  }
 
   // Arrow function should be used to execute method with correct context
-  const changeNamesHandler = () => {
-    // 'personStateSetter' is a function that must be used to change 'personState'
-    // it create a new state (info: "Very important info" will be removed)
-    personStateSetter({
+  changeNamesHandler = () => {
+    // 'setState' is a special function that must be used to change 'state'
+    // it merges new state with previous (info: "Very important info" won't be changed)
+    this.setState({
       persons: [
         {name: "NIKITA", age: 25},
         {name: "BOBA", age: 23},
@@ -29,23 +28,38 @@ const App = () => {
     })
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={changeNamesHandler}>Uppercase Names</button>
-        {/* Adding component just as an HTML element */}
-        <Person 
-          name={personState.persons[0].name} 
-          age={personState.persons[0].age}/>
-        <Person 
-          name={personState.persons[1].name} 
-          age={personState.persons[1].age}/>
-        <Person 
-          name={personState.persons[2].name} 
-          age={personState.persons[2].age}/>
-      </header>
-    </div>
-  );
+  // Method to dinamically change output when changing input
+  changeNameHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: event.target.value, age: 25},
+        {name: "BOBA", age: 23},
+        {name: "BANKSY", age: 3}
+      ],
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.changeNamesHandler}>Uppercase Names</button>
+          {/* Adding component just as an HTML element */}
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}
+            changedName={this.changeNameHandler}/> {/* method is passed to component related to person[0] - Nikita */}
+                                                   {/* person[0].name changes as input changes */}
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}/>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- guide/src/Person/Person.js
+++ guide/src/Person/Person.js
@@ -4,6 +4,7 @@ const person = props => {
   return (
     <div>
       <p>I'm {props.name}, {props.age} years old.</p>
+      <input type="text" value={props.name} onChange={props.changedName}/>
     </div>
   )
 }
