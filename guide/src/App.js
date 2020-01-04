73d271cbf0505d92db6c467d6644ef0a7d80d5b5
kotov <kotov.nikita.sergeevich@gmail.com>
2020-01-04 21:45:57 +0300

    Adding stateless component to root stateful component

--- guide/src/App.js
+++ guide/src/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
+// Import stateless component "person"
+import Person from './Person/Person';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          
+          {/* Adding component just as an HTML element */}
+          <Person name="Nikita" age="25"/>
+          <Person name="Boba" age="23"/>
+          <Person name="Banksy" age="4"/>
         </header>
       </div>
     );
