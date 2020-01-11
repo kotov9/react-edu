c51bd6e1a83ff8e3852589af0d70e7125f386960
kotov <kotov.nikita.sergeevich@gmail.com>
2020-01-11 23:02:04 +0300

    Using hoc to enclose jsx elements that must be rendered

--- guide/src/components/Persons/Person/Person.js
+++ guide/src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React from 'react';
 // Import css file to use styling of component from it
 // * in this case styling is avaliable globally (in any place this file is imported)
 import './Person.css';
+import Aux from '../../../hoc/Aux';
 
 
 const person = props => {
@@ -9,10 +10,10 @@ const person = props => {
   return (
     // place few jsx in array so they could be rendered without use of one all enclosing extra div
     // each element in array MUST have unique key
-    [
-      <p key="i1" onClick={props.clicked}>I'm {props.name}, {props.age} years old.</p>,
+    <Aux>
+      <p key="i1" onClick={props.clicked}>I'm {props.name}, {props.age} years old.</p>
       <input key="i2" type="text" value={props.name} onChange={props.changed}/>
-    ]
+    </Aux>
   )
 }
 
---
+++ guide/src/hoc/Aux.js
@@ -0,0 +1,5 @@
+// Higher order component to enclose jsx elements
+// without using extra div element
+const Aux = props => props.children;
+
+export default Aux;
