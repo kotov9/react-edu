06aa198bca8e5fc4c85509afff3ce602fdce061c
kotov <kotov.nikita.sergeevich@gmail.com>
2020-01-12 14:23:33 +0300

    Passing unknown props to the wrapping HOC

--- guide/src/components/Persons/Person/Person.js
+++ guide/src/components/Persons/Person/Person.js
@@ -2,7 +2,9 @@ import React from 'react';
 // Import css file to use styling of component from it
 // * in this case styling is avaliable globally (in any place this file is imported)
 import styles from './Person.module.css';
-import WithClass from '../../../hoc/WithClass';
+
+import withClass2 from '../../../hoc/withClass2';
+import Aux from '../../../hoc/Aux';
 
 
 const person = props => {
@@ -10,12 +12,12 @@ const person = props => {
   return (
     // place few jsx in array so they could be rendered without use of one all enclosing extra div
     // each element in array MUST have unique key
-    <WithClass classes={styles.Person}>
+    <Aux>
       <p key="i1" onClick={props.clicked}>I'm {props.name}, {props.age} years old.</p>
       <input key="i2" type="text" value={props.name} onChange={props.changed}/>
-    </WithClass>
+    </Aux>
   )
 }
 
 
-export default person;
+export default withClass2(person, styles.Person);
