986a85047abfb0b9e3932510bf6a407e115fac45
Nikita M <morevnsw@gmail.com>
2020-03-02 11:19:41 +0300

    Adding Switch to choose the first matched route, add params to url and way of extracting it

--- ajaxPost/src/containers/Blog/Blog.js
+++ ajaxPost/src/containers/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 
 class Blog extends Component {
@@ -30,8 +31,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" exact component={NewPost}/>
+                <Switch>
+                    <Route path="/" exact component={Posts}/>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
