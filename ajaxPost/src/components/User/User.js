df48ed8f042790b2307b5023a09176caa18cb38b
Nikita M <morevnsw@gmail.com>
2020-03-02 14:23:03 +0300

    Use Suspense and React.lazy to execute lazy loading

---
+++ ajaxPost/src/components/User/User.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const User = props => {
+  return <h1>User Loaded</h1>;
+}
+
+
+export default User;
--- ajaxPost/src/containers/Blog/Blog.js
+++ ajaxPost/src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 
@@ -6,7 +6,9 @@ import './Blog.css';
 import Posts from './Posts/Posts';
 // import NewPost from './NewPost/NewPost';
 import AsyncComponent from '../../hoc/AsyncComponent';
-const AsyncNewPost = AsyncComponent(() => import('./NewPost/NewPost'))
+const AsyncNewPost = AsyncComponent(() => import('./NewPost/NewPost'));
+const AsyncUser = React.lazy(() => import('../../components/User/User'));
+
 
 class Blog extends Component {
     state = {
@@ -31,12 +33,23 @@ class Blog extends Component {
                                 hash: '#idToJumpTo',
                                 search: '?quick-submit=true'
                             }}>New Post</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/user'
+                            }}>User</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 <Switch>
                     <Route path="/posts" component={Posts} />
                     {this.state.isAuth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+                    <Route 
+                        path="/user" 
+                        render={() => {
+                            return <Suspense fallback={<div>Loading...</div>}>
+                                <AsyncUser />
+                            </Suspense>
+                        }
+                    } />
                     {/* <Route render={() => <h1>Not Found</h1>} /> */}
                     <Redirect from="/" to="/posts" />
                 </Switch>
