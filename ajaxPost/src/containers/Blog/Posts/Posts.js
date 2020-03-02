21b6a1e1d752530acf066337164cb0f0ceaa3e0d
Nikita M <morevnsw@gmail.com>
2020-03-02 11:27:22 +0300

    Using built-in func to go to given path (instead of using Link) when clicking the post

--- ajaxPost/src/containers/Blog/Posts/Posts.js
+++ ajaxPost/src/containers/Blog/Posts/Posts.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Post from '../../../components/Post/Post';
 import axios from '../../../axios';
 import './Posts.css';
-import { Link } from 'react-router-dom';
 
 
 class Posts extends Component {
@@ -28,7 +27,7 @@ class Posts extends Component {
     }
 
     postSelectedHandler = id => {
-        this.setState({ selectedPostId: id });
+        this.props.history.push({pathname: '/' + id})
     }
     
 
@@ -37,12 +36,11 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link key={post.id} to={"/" + post.id}>
-                        <Post
-                            title={post.title}
-                            author={post.author}
-                            clicked={() => this.postSelectedHandler(post.id)} />
-                    </Link>
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)} />
                 )
             })
         }
