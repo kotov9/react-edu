2315b0773d7e34bba2bac6d26815cc74f5c51a6c
Nikita M <morevnsw@gmail.com>
2020-03-11 23:05:56 +0300

    Redux main steps example

---
+++ redux-start/src/redux-basis.js
@@ -0,0 +1,42 @@
+const redux = require('redux');
+
+const initialState = {
+  counter: 0,
+}
+
+
+// Reducer
+const reducer = (state=initialState, action) => {
+  switch (action.type) {
+    case 'INC_COUNTER':
+      return {
+        ...state,
+        counter: state.counter + 1
+      }
+    case 'ADD_COUNTER':
+      return {
+        ...state,
+        counter: state.counter + 10
+      }
+    default:
+      return state;
+  }
+}
+
+// Store
+const store = redux.createStore(reducer);
+console.log(store.getState());
+
+// Subscriptions
+store.subscribe(() => {
+  console.log("[Subscription]:", store.getState())
+})
+
+
+// Dispatching action
+store.dispatch({type: 'INC_COUNTER'});
+console.log("Wait...wait...wait...")
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+// console.log(store.getState())
+
+
