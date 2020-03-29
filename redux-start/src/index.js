4cba5df48b8ebf4f5c636acb49dcd153e5514e77
Nikita M <morevnsw@gmail.com>
2020-03-29 19:35:42 +0300

    Adding simple midleware to redux

--- redux-start/src/index.js
+++ redux-start/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -16,7 +16,18 @@ const rootReducer = combineReducers({
     res: resultReducer
 })
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware]: action:', action);
+            const result = next(action);
+            console.log('[Middleware]: next state:', store.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
