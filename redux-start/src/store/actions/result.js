6bb39c7a7206f8425b028bfeb15d78e0980f1a59
Nikita M <morevnsw@gmail.com>
2020-03-30 12:04:58 +0300

    Adding fallback to action creater - getting prev state before updating it in reducer in case of async operations (storeResult example)

--- redux-start/src/store/actions/result.js
+++ redux-start/src/store/actions/result.js
@@ -8,8 +8,9 @@ export const saveResult = result => {
 }
 
 export const storeResult = (result) => {
-  return dispatch => {
+  return (dispatch, getState) => {
     setTimeout(() => {
+      console.log(getState().ctr.counter);
       dispatch(saveResult(result))
     }, 3000)
   }
