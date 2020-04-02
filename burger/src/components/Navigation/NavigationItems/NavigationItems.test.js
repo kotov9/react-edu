876b62faf895c04462d55fa387b770f907d9b590
Nikita M <morevnsw@gmail.com>
2020-04-02 22:55:36 +0300

    Added more improved tests to component (NavigationItems)

--- burger/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,16 +8,26 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 configure({adapter: new Adapter()});
 
+let wrapper;
+
+beforeEach(() => {
+  // 'shallow' NOT deeply renders component (content of nested components is rendered as placeholder)
+  wrapper = shallow(<NavigationItems />); 
+})
+
 describe('<NavigationItems />', () => {
   it('should render two <NavigationItem /> elements if not authenticated', () => {
-    // 'shallow' NOT deeply renders component (content of nested components is rendered as placeholder)
-    const wrapper = shallow(<NavigationItems />);
     // find method help to define if component has content defined as argument of find
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
   })
   
   it('should render three <NavigationItem /> elements if authenticated', () => {
-    const wrapper = shallow(<NavigationItems isAuth/>);
+    wrapper.setProps({isAuth: true});
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  })
+  
+  it('should render logout <NavigationItem /> element if authenticated', () => {
+    wrapper.setProps({isAuth: true});
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
   })
 })
